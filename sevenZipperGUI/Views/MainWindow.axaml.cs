27c0356350b5d3e2b48bf8687227da24f438bc35
Bate <td.zazsa@gmail.com>
2026-07-21 18:31:51 +0200

    pick files and folders

--- sevenZipperGUI/Views/MainWindow.axaml.cs
+++ sevenZipperGUI/Views/MainWindow.axaml.cs
@@ -1,11 +1,52 @@
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using System;
 
 namespace sevenZipperGUI.Views;
+
 
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    async public void OpenFile(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        if (topLevel == null) return;
+
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Select a File",
+            AllowMultiple = false,
+        });
+
+        if (files.Count >= 1)
+        {
+            var selectedFile = files[0];
+            string filePath = selectedFile.Path.LocalPath;
+            Console.WriteLine(filePath);
+        }
+    }
+
+    async public void OpenFolder(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        if (topLevel == null) return;
+
+        var folders = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+        {
+            Title = "Select a Folder",
+            AllowMultiple = false
+        });
+
+        if (folders.Count >= 1)
+        {
+            var selectedFolder = folders[0];
+            string filePath = selectedFolder.Path.LocalPath;
+            Console.WriteLine(filePath);
+        }
     }
 }
